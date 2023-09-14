e906dc044c4af12565c4d8719ec13416cc7294d7
DataSpot42 <stevelookingforward@gmail.com>
2023-09-14 11:19:04 +0100

    basket update

---
+++ frontend/src/components/orderNumGen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { useEffect, useState } from 'react';
+
+export const BasketInit = (custNum) => {
+    const [orderNumber, setOrderNumber] = useState([])
+    
+}
