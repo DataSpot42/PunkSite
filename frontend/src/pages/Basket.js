f1cea2b97d571aa58a4a3f6da2d9ff8bb52abaa3
DataSpot42 <stevelookingforward@gmail.com>
2023-09-18 10:30:14 +0100

    Added some comments and tidied up

--- frontend/src/pages/Basket.js
+++ frontend/src/pages/Basket.js
@@ -1,5 +1,5 @@
 import { useEffect, useState, React } from "react";
-import { readPunks} from "../api/readPunks";
+import { readPunks } from "../api/readPunks";
 import { getPunk } from "../api/getPunk"
 import Card from '../components/Card'
 import { deletePunk } from "../api/deletePunk";
@@ -13,86 +13,62 @@ import { useNavigate } from "react-router-dom"
 
 const Basket = () => {
     console.log(`Welcome to the Basket`)
-    const [punks, setPunks] = useState()    
-    const { id } = useParams()    
-    
-    const navigate = useNavigate() 
-    
+    const [punks, setPunks] = useState()
+    const { id } = useParams()
+    const navigate = useNavigate()
     const handlerBackToShop = () => {
-    
-    navigate (`/Shop/${id}`)     
+        navigate(`/Shop/${id}`)   //navigate back to shop with current basket id
     }
-    
+
     const deleteHandler = async (punk) => {
-        console.log(punk,id)
-        deletePunk(punk,id) 
+        deletePunk(punk, id)     // deletes item from database
+        let updatedPunks = await getPunk(id)
+        updatedPunks = await getPunk(id)  //updates basket display
+        console.log(updatedPunks)
+        setPunks(updatedPunks.items)
+    };
+
+    const addQPunkHandler = async (punk) => {
+        addQPunk(punk, id)              // adds 1 to quanitiy
         let updatedPunks = await getPunk(id)
         updatedPunks = await getPunk(id)
-        console.log (updatedPunks)
-        setPunks(updatedPunks.items) 
-       };
-    
-       const addQPunkHandler = async (punk) => {
-        
-        addQPunk(punk,id) 
-        let updatedPunks = await getPunk(id)
-        updatedPunks = await getPunk(id)        
-        setPunks(updatedPunks.items)    
-       };   
+        setPunks(updatedPunks.items)
+    };
 
-       const subQPunkHandler = async (punk) => {
-        subQPunk(punk,id) 
+    const subQPunkHandler = async (punk) => {
+        subQPunk(punk, id)              //subtracts 1 form quanitiy
         let updatedPunks = await getPunk(id)
         updatedPunks = await getPunk(id)
-        console.log (updatedPunks)
-        setPunks(updatedPunks.items) 
-        console.log(punk)
-        
-        if (punk.quantity === 1) { console.log(`Zero!`); deleteHandler(punk,id) }    
-        
-    
-        console.log(punk)
-       
-       };   
+        setPunks(updatedPunks.items)
 
+        if (punk.quantity === 1) { deleteHandler(punk, id) }
+    };
 
     useEffect(() => {
         const fetchPunks = async () => {
             let data = await getPunk(id)
-            setPunks(data.items)            
+            setPunks(data.items)
         }
         fetchPunks()
     }, [])
-
-    console.log(punks)
     let total = 0
-    
-    
     if (!punks) return <h1>loading...</h1>
-    for (let k=0; k<punks.length; k++) {
-        total = total + (punks[k].price * punks[k].quantity)
+    for (let k = 0; k < punks.length; k++) {
+        total = total + (punks[k].price * punks[k].quantity)  // works out price
     }
-    console.log(total)
-
     return (
         <div className="toDoItems">
-            <button className="btnLinks" onClick={()=>handlerBackToShop()}>Back to Shop</button>
+            <button className="btnLinks" onClick={() => handlerBackToShop()}>Back to Shop</button>
             <>
                 {
-                    punks ? 
-
-                    
-                    
-                    punks.map((punk,index) => <Card key={punk.id} deleteHandler={deleteHandler} addQPunkHandler={addQPunkHandler} subQPunkHandler={subQPunkHandler}  punk={punk}/>)
-                        : <p>loading...</p>
+                    punks ?
+                        punks.map((punk, index) => <Card key={punk.id} deleteHandler={deleteHandler} addQPunkHandler={addQPunkHandler} subQPunkHandler={subQPunkHandler} punk={punk} />)
+                        : <p>loading...</p>   //displays basket items
                 }
             </>
-            
-            <h1> Total £{total}</h1>
+            <h1> Total £{total}</h1> 
         </div>
     );
-
 }
-
 export default Basket
 
--- frontend/src/pages/Shop.js
+++ frontend/src/pages/Shop.js
@@ -18,19 +18,12 @@ import ad from '../images/adbanner.jpg';
 
 const Beer = () => {
   const { id } = useParams()
-  const [toUpdate, setToUpdate] = useState('')
-  
-  const [btnPopup, setBtnpopup] = useState(false)
   const navigate = useNavigate()
   const [item, setItem] = useState([]);
-  const [orderItem, setOrderItem] = useState(0);
-  const [basket, setBasket] = useState([]);
   const [start, setStart] = useState(0);
   const [end, setEnd] = useState(10);
-  const orderNum = faker.number.int({ min: 1, max: 100 })
-  const custNum = 1
   let itemNum = 1
-  let basketItem =[]
+  let basketItem = []
   console.log('Welcome to the Shop')
   const handlerNextPage = () => {
     setStart(start + 10)
@@ -44,126 +37,88 @@ const Beer = () => {
     let obj2 = await getPunk(id)  // get curent order 
     let array = []
     itemNum++
-    let amount=1
-    array = [product]  
+    let amount = 1
+    array = [product]
     let qtyFlag = 0
-    let obj = { /* _id:id,  */ /* orderNum: ourBasket.orderNum,
-    custName: ourBasket.custName, */
-    items: [{
-        item: 1,    
+    let obj = {
+      items: [{
+        item: 1,
         productID: product.id,
         productName: product.name,
         productImage: product.image_url,
         quantity: amount,
-        price: product.price
-}]}  
-console.log(obj)
-console.log(obj2)
-// format chosen product in the right form to go into the basket
-for (let k=0; k<obj2.items.length; k++) {  
-if (obj.items[0].productID === obj2.items[k].productID) {obj2.items[k].quantity++; qtyFlag++}}
-console.log(qtyFlag)
-if (qtyFlag===0){
-  obj2.items.push(obj.items[0])} //add current items to new item
-  
-  let response = await editPunk(obj2,id)         
-  
-   
-} 
-  const handlerGotoBasket = (e) => {
-    
-    navigate (`/Basket/${id}`) 
+        price: product.price  //format product into Schema layout
+      }]
+    }
+    for (let k = 0; k < obj2.items.length; k++) {
+      if (obj.items[0].productID === obj2.items[k].productID) { obj2.items[k].quantity++; qtyFlag++ }
+    }
+    console.log(qtyFlag)  //check if this item is already in the basket, if so add to quantity
+    if (qtyFlag === 0) {
+      obj2.items.push(obj.items[0])
+    } // if not add new item to current itams
+
+    let response = await editPunk(obj2, id)  // store updated basket in database    
+
   }
+  const handlerGotoBasket = (e) => {
 
-  
-  
-
-
+    navigate(`/Basket/${id}`)
+  }
 
   const getBeer = async () => {
     const response = await fetch("https://api.punkapi.com/v2/beers?page=2&per_page=80");
     const data = await response.json();
-    
-    
-   
-   const pricedData = data.map((price) => {
-    return {
-        price: parseFloat(faker.commerce.price({ min: 10, max: 30, dec: 2})),
+    const pricedData = data.map((price) => {
+      return {
+        price: parseFloat(faker.commerce.price({ min: 10, max: 30, dec: 2 })),
         ...price
-    }
-});
-
-  
-setItem(pricedData)
-
+      }
+    });
+   setItem(pricedData)   // add prices to API product data
   }
 
   useEffect(() => {
     getBeer();
   }, []);
 
-
-
-  return (
-    <div>
-    <img className='adbanner' src={ad} alt="Logo"></img>
-    <div className='contianer'>
-     
-    
-      <div className="cards-grid-wrap">
-    
-        {
-          item.slice(start, end).map((info, index) => {
-            return (
-
-              <div className="card_item" key={info.id}>
-                <div className="card_inner">
-                  <img className='card_img' src={info.image_url} alt="" />
-                </div>
-
-                  
-                    <div className='txtcard'> 
+  return (   
+    <div>     
+      <img className='adbanner' src={ad} alt="Logo"></img>
+      <div className='contianer'>
+        <div className="cards-grid-wrap">
+          {
+            item.slice(start, end).map((info, index) => {
+              return (
+                <div className="card_item" key={info.id}>
+                  <div className="card_inner">
+                    <img className='card_img' src={info.image_url} alt="" />
+                  </div>
+                  <div className='txtcard'>
                     <div className="gitDetail textcard avb">avb - {info.abv}%</div>
                     <div className="gitDetail textcard vol">{info.volume.value} liters</div>
-                    </div>
-                    <div className="detail-box">
-                    
+                  </div>
+                  <div className="detail-box">
                     <div className="beerName">{info.name}</div>
                     <div className="gitDetail"> {info.ingredients.malt[0].name}</div>
                     <div className="gitDetail">£{info.price}</div>
                   </div>
+                  <button onClick={() => handlerAddBasket(info)} className="seeMore"> Add</button>                 
 
-                  <button onClick={() => handlerAddBasket(info)} className="seeMore"> Add</button>
-                  {/* <button onClick={() => handlerPopup(info)}> More Info</button> */}
-                  
-                  
-    <Popup  trigger={<button>More Info</button>} position="right center">
-      <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} className="popupclass" >{info.name} {info.description} </motion.div>
-    </Popup>
- 
-                  {/* {setBtnpopup && < Popup productinfo = {info} /> } */}
-                
+                  <Popup trigger={<button>More Info</button>} position="right center">
+                    <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} className="popupclass" >{info.name} {info.description} </motion.div>
+                  </Popup>                 
 
-
-              </div>
-            )
-          })
-        }
-        <button onClick={() => handlerNextPage()}> Next Page</button>
-        <button onClick={(e) => handlerGotoBasket(e.target.value)}> Basket</button>
-
-        
-
-       {/* {<Popup trigger={btnPopup} setTrigger={setBtnpopup}>
-
- </Popup> } */}
+                </div>
+              )
+            })
+          }
+          <button onClick={() => handlerNextPage()}> Next Page</button>
+          <button onClick={(e) => handlerGotoBasket(e.target.value)}> Basket</button>          
+        </div>
       </div>
-
-    </div>
     </div>
   );
-
-
 }
 
 export default Beer
